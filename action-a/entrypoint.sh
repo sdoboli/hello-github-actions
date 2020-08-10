b00c4f740c95f5e9a93bd94e99e2627506589e87
Simona Doboli <simona.doboli@hofstra.edu>
2020-08-10 13:42:48 -0400

    Update entrypoint.sh

--- action-a/entrypoint.sh
+++ action-a/entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/sh -l
 
 sh -c "echo Hello world my name is $INPUT_MY_NAME"
-git clone https://github.com/google/googletest.git
-#apt-get install libgtest-dev
+#git clone https://github.com/google/googletest.git
+apt install libgtest-dev
 cd /usr/src/gtest
 cmake CMakeLists.txt
 make
